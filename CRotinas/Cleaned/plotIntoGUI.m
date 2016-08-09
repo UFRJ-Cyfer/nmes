1a14037ce5cdb4936dd4e50464197fa914b01e93
UFRJCyfer <luizrennocosta@poli.ufrj.br>
2016-08-09 15:14:59 -0300

    Fix

--- CRotinas/Cleaned/plotIntoGUI.m
+++ CRotinas/Cleaned/plotIntoGUI.m
@@ -1,7 +1,8 @@
 user = handles.timeData.timeResponse(:,1);
 ref = handles.timeData.timeResponse(:,2);
-time = (handles.timeData.timeResponse(:,4) - ...
-    handles.timeData.timeResponse(1,4))/1000;
+
+time = (handles.timeData.timeResponse(:,3) - ...
+    handles.timeData.timeResponse(1,3))/1000;
 
 idx = find(ref>150);
 ref(idx)= ref(idx-1);
@@ -9,4 +10,10 @@ idx = find(user>150);
 user(idx)= user(idx-1);
 
 plot(handles.timePlot,time,user,'b','LineWidth',2);hold on
-plot(handles.controlPlot,time,ref,'r--','LineWidth',2);hold off
+plot(handles.timePlot,time,ref,'r--','LineWidth',2);hold off
+
+biceps = handles.timeData.timeResponse(:,5);
+triceps = handles.timeData.timeResponse(:,6);
+
+plot(handles.controlPlot,time,biceps,'b','LineWidth',2);hold on
+plot(handles.controlPlot,time,triceps,'r--','LineWidth',2);hold off
